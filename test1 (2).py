2ffbeb293b05702baea4e6b44c0231a6439a0d46
A Person <18553610+boldcounselor@users.noreply.github.com>
2018-08-28 16:43:29 +0800

    dab

--- test1 (2).py
+++ test1 (2).py
@@ -1,22 +1,21 @@
 import math
-import numpy as np
 import random
 import string
-import matplotlib.pyplot as mpl
 
 
 class QuestionAsker:
-    def __init__(self, question_set):
-        self.parent_question = None
-        self.question_set = question_set
+    def __init__(self, question_set, askedQuestions=[]):
         if(question_set):
+            self.askedQuestions=askedQuestions
+            self.question_set = question_set
             self.question = self.find_question()
             self.answer_set = self.find_answer_set(self.question)
             self.fpve_set = self.find_false_positive(self.question)
             self.fnve_set = self.find_false_negative(self.question)
-            self.fpve_question = QuestionAsker(self.fpve_set)
+            self.askedQuestions.append(self.question)
+            self.fpve_question = QuestionAsker(self.fpve_set, self.askedQuestions)
             ##elf.fpve_question.parent_question=self
-            self.fnve_question = QuestionAsker(self.fnve_set)
+            self.fnve_question = QuestionAsker(self.fnve_set, self.askedQuestions)
             ##self.fpve_question.parent_question=self
 
     def find_answer_set(self, question):
@@ -92,14 +91,17 @@ class QuestionAsker:
 
     def find_question(self):
         if (self.question_set != []):
+            question_blacklist = []
             best_question_fitness = float('-inf')
             best_question = []
             for i in range(trait_no):
                 for j in range(trait_no):
-                    if (self.fitness([i, j, 0]) > best_question_fitness):
+                    if (self.fitness([i, j, 0]) > best_question_fitness and self.find_answer_set([i, j, 0]) != False and ([i, j, 0] or [j, i, 0]) not in self.askedQuestions):
+                        print(bool(self.find_answer_set([i, j, 0])))
                         best_question = [i, j, 0]
                         best_question_fitness=self.fitness([i, j, 0])
-                    if (self.fitness([i, j, 1]) > best_question_fitness):
+                    if (self.fitness([i, j, 1]) > best_question_fitness and self.find_answer_set([i, j, 1]) != False and ([i, j, 1] or [j, i, 1]) not in self.askedQuestions):
+                        print(bool(self.find_answer_set([i, j, 0])))
                         best_question = [i, j, 1]
                         best_question_fitness=self.fitness([i, j, 1])
             return best_question
@@ -108,14 +110,19 @@ class QuestionAsker:
 
 
 trait_no = 8
-breed_no = 5
-trait_breed = [[True, False, True, False, True, False, True, False],
-               [True, False, True, False, True, True, True, False],
+breed_no = 8
+trait_breed = [[True, False, True, True, True, True, True, False],
+               [True, False, True, False, True, True, True, True],
+               [False, False, True, False, True, True, True, True],
+               [True, True, True, True, True, False, True, True],
+               [True, False, True, False, True, False, True, True],
                [False, False, True, False, True, False, False, True],
                [True, True, True, False, True, False, True, True],
-               [True, False, True, False, True, False, False, True]]
+               [True, False, False, False, True, False, False, False]]
+
 breed_set = [[0, 1, 2], [3, 4]]
-grandcanyon = QuestionAsker([1, 2, 3])
+grandcanyon = QuestionAsker([1, 3, 5])
+print("Done!")
 
 
 
