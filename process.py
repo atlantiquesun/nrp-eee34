37b1280d17cacaad95d02d789e9c4c3c8358b9c0
Conrad <18553610+boldcounselor@users.noreply.github.com>
2018-10-10 23:50:12 +0800

    ry eryutrtd

--- process.py
+++ process.py
@@ -5,6 +5,7 @@ import numpy as np
 from PyQt5.QtWidgets import QApplication, QLabel
 
 #Hey Yi Ran! This is the code for the basic program I've made.
+#Images to be tested are placed in the ./doggos folder and are in jpg format and labelled numerically zero-indexed.
 #I'm using PyQT for the GUI and I'm
 
 #This function takes in a question written in Reverse Polish Notation and returns you the breeds for which you get a True/False answer!
@@ -23,7 +24,29 @@ def check_answers(trait_breed,question):
             stack.append(trait_breed[:,i])
     return stack
 
-#This generates a boolean breed-trait matrix!
+#Takes in the question and the trait_info list, a list of arrays of the trait number, the corresponding trait and the grammar classes it falls under.
+#For instance, [2, long legs, plural] or [5, a nice personality, singular]
+#Quite basic, feel free to edit if you want to!
+#I've set it up to be able to handle one or two-word questions
+#trait_info is a
+def rpn_to_english(trait_info,question):
+    print("This dog " + description + ".")
+    description=""
+    for i in range(len(question)):
+        str1 = ""
+        str2 = ""
+        if (question[i] == "NOT"):
+            stack.append(np.invert(stack.pop()))
+        elif (question[i] == "AND"):
+            stack.append(np.logical_and(stack.pop(),stack.pop()))
+        elif (question[i] == "OR"):
+            stack.append(np.logical_or(stack.pop(),stack.pop()))
+        else:
+            if str1==""
+            stack.append(trait_breed[:,i])
+    return stack
+
+#This generates a random boolean breed-trait matrix for mass-testing!
 def generate_breed_trait_matrix(trait_no,breed_no):
     trait_breed=[]
     for _ in range(breed_no):
