713ea7fefb0e124ecb7282e1d8ec03cc0a879c28
Conrad <18553610+boldcounselor@users.noreply.github.com>
2018-10-13 17:11:26 +0800

    53534t
    tretretre

--- process.py
+++ process.py
@@ -2,7 +2,10 @@ import math
 import random
 import string
 import numpy as np
-from PyQt5.QtWidgets import QApplication, QLabel
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton
+from PyQt5.QtGui import QIcon
+from PyQt5.QtCore import pyqtSlot
 
 #Hey Yi Ran! This is the code for the basic program I've made.
 #Images to be tested are placed in the ./doggos folder and are in jpg format and labelled numerically zero-indexed.
@@ -27,24 +30,12 @@ def check_answers(trait_breed,question):
 #Takes in the question and the trait_info list, a list of arrays of the trait number, the corresponding trait and the grammar classes it falls under.
 #For instance, [2, long legs, plural] or [5, a nice personality, singular]
 #Quite basic, feel free to edit if you want to!
-#I've set it up to be able to handle one or two-word questions
-#trait_info is a
-def rpn_to_english(trait_info,question):
-    print("This dog " + description + ".")
-    description=""
-    for i in range(len(question)):
-        str1 = ""
-        str2 = ""
-        if (question[i] == "NOT"):
-            stack.append(np.invert(stack.pop()))
-        elif (question[i] == "AND"):
-            stack.append(np.logical_and(stack.pop(),stack.pop()))
-        elif (question[i] == "OR"):
-            stack.append(np.logical_or(stack.pop(),stack.pop()))
-        else:
-            if str1==""
-            stack.append(trait_breed[:,i])
-    return stack
+#Right now, all it does is just return the question typed in as a string. I don't know how it should be converted haha.
+def rpn_to_english(trait_info, question):
+    string=""
+    for i in question:
+        string+=str(i)+" "
+    return string
 
 #This generates a random boolean breed-trait matrix for mass-testing!
 def generate_breed_trait_matrix(trait_no,breed_no):
@@ -60,11 +51,52 @@ def generate_breed_trait_matrix(trait_no,breed_no):
 
 tb_matrix=generate_breed_trait_matrix(2,4)
 
-app = QApplication([])
-label = QLabel('Hello World!')
-label.show()
-app.exec_()
+class UlamRenyi(self):
+    def question(state_sigma):
+        num_error=len(state_sigma)-1
+        question=np.zeros(1,num_error+1)
+        if np.sum(state_sigma)== (1 or 0)
+            print("Final state, check the answer. Yeet -Qi Yu our dank master PhD student")
+        else
+            for i in range(0,num_error)
+                diff=np.zeros(1,state_sigma[i]+1)
+                state_i_sigma = np.concatenate(np.zeros(1, num_error-i),state_sigma[0:i])
+                for j = range(0,state_sigma[i])
+                    ques_i = np.zeros(1, num_error - i) question(1:max(0, i)) j];
+                    [state_i_yes, state_i_no] = StateAfterQues(state_i_sigma, ques_i);
+                    diff(j + 1) = abs(Weight(state_i_yes, gamma(i + 1) - 1) - Weight(state_i_no, gamma(i + 1) - 1));
+class ResponseApp(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.title = 'PyQt5 button - pythonspot.com'
+        self.left = 10
+        self.top = 10
+        self.width = 800
+        self.height = 800
+        self.initUI()
 
+    def initUI(self):
+        self.setWindowTitle(self.title)
+        self.setGeometry(self.left, self.top, self.width, self.height)
+
+        button = QPushButton('PyQt5 button', self)
+        button.setToolTip('This is an example button')
+        button.move(100, 70)
+        button.clicked.connect(self.on_click)
+
+        self.show()
+
+    @pyqtSlot()
+    def on_click(self):
+        print('PyQt5 button click')
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = ResponseApp()
+    while(True):
+        print("test")
+    sys.exit(app.exec_())
 
 
 
